453dd20b8d1648c83189bbdb68d2206b88563f80
Johstin <johstiin@gmail.com>
2024-05-01 18:59:01 -0500

    Punto 5

--- punto5.js
+++ punto5.js
@@ -1,1 +1,7 @@
-//ingreso de colaborador
+function circulo(radio){
+    var area = Math.PI*radio*radio;
+    return area;
+}
+ const radio = 10;
+ const area = circulo(radio)
+ console.log("El area del circulo es", area);
